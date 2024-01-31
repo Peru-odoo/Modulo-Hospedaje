45b07a096ff6e67992b3170c916495af118ff867
YankielYong <yankielyongmartinez@gmail.com>
2024-01-31 14:45:07 -0500

    agregado campo de imagen y la vista kanban para habitaciones

--- models/habitacion.py
+++ models/habitacion.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
-from email.policy import default
+import base64
 import logging
 
-from odoo import models, fields, api, _
+from odoo import models, fields, api, _, modules
 from odoo.exceptions import UserError, ValidationError
-from odoo.tools.populate import compute
 
 _logger = logging.getLogger(__name__)
 
+def _get_default_image():
+        image_path = modules.get_module_resource('hotel', 'static/src/img', 'habitacion.png')
+        return base64.b64encode(open(image_path, 'rb').read())
 
 class Habitacion(models.Model):
     _name = 'hotel.habitacion'
@@ -28,6 +30,7 @@ class Habitacion(models.Model):
     capacidad = fields.Integer(string=_('Capacidad'), compute='_compute_capacidad', default="0")
     currency_id = fields.Many2one('res.currency', 'Moneda', required=True, default=lambda self: self.env['res.currency'].search([('name', '=', 'CUP')]).id)
     precio_por_noche = fields.Monetary(string=_('Precio por noche'), required=True)
+    image = fields.Binary(string=_('Imagen'), default=_get_default_image())
 
     _sql_constraints = [('numero_unico', 'unique (numero)', 'Ya existe una habitación con este número')]
     
