ba989cec9959625c6a2a113691d1f00734b5aec4
YankielYong <yankielyongmartinez@gmail.com>
2024-02-03 17:20:25 -0500

    actualizada vista kanban y se agrego el chatter

--- models/habitacion.py
+++ models/habitacion.py
@@ -13,14 +13,16 @@ def _get_default_image():
 
 class Habitacion(models.Model):
     _name = 'hotel.habitacion'
+    _inherit = ['mail.thread']
     _description = _('Habitación del hotel')
-    _order = 'name'
+    _order = 'numero'
 
-    name = fields.Char(string=_('Nº'), default="Nueva Habitación")
-    numero = fields.Integer(string=_('Número'), required=True)
+    name = fields.Char(string=_('Nº'), compute="_compute_name", default="Nueva Habitación", tracking=True)
+    numero = fields.Integer(string=_('Número'), required=True, tracking=True)
     tipo = fields.Selection(
         string=_('Tipo de habitación'),
         required=True,
+        tracking=True,
         selection=[
             ('simple', 'Simple'),
             ('doble', 'Doble'),
@@ -30,30 +32,24 @@ class Habitacion(models.Model):
     )
     capacidad = fields.Integer(string=_('Capacidad'), compute='_compute_capacidad', default="0")
     currency_id = fields.Many2one('res.currency', 'Moneda', required=True, default=lambda self: self.env['res.currency'].search([('name', '=', 'CUP')]).id)
-    precio_por_noche = fields.Monetary(string=_('Precio por noche'), required=True)
-    image = fields.Binary(string=_('Imagen'), default=_get_default_image())
+    precio_por_noche = fields.Monetary(string=_('Precio por noche'), required=True, tracking=True)
+    image = fields.Binary(string=_('Imagen'), default=_get_default_image(), tracking=True)
     estado = fields.Selection(
         string=_('Estado'),
         selection=[
             ('disponible', 'Disponible'),
             ('ocupada', 'Ocupada'),
         ],
-        default="disponible"
+        default="disponible", tracking=True
     )
 
     _sql_constraints = [('numero_unico', 'unique (numero)', 'Ya existe una habitación con este número')]
-    
-    @api.model_create_multi
-    def create(self, vals_list):
-        for vals in vals_list:
-            vals['name'] = f"Habitación {vals['numero']}"
-        return super(Habitacion, self).create(vals_list)
-    
-    # @api.depends('numero')
-    # def _compute_name(self):
-    #     for rec in self:
-    #         rec.name = f'Habitación {rec.numero}'
             
+    @api.depends('numero')
+    def _compute_name(self):
+        for rec in self:
+            rec.name = f"Habitación {rec.numero}"
+        
     @api.depends('tipo')
     def _compute_capacidad(self):
         for rec in self:
@@ -73,5 +69,11 @@ class Habitacion(models.Model):
         for rec in self:
             if rec.precio_por_noche <= 0:
                 raise ValidationError('El precio por noche debe ser mayor que 0')
+            
+    @api.constrains('numero')
+    def _check_numero(self):
+        for rec in self:
+            if rec.numero < 1:
+                raise ValidationError('El número de la habitación debe ser mayor que 0')
         
     
