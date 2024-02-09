111d1235f1117e9f7ab403ce04ab830db2739d08
YankielYong <yankielyongmartinez@gmail.com>
2024-02-09 13:21:40 -0500

    automatizado el proceso de asignacion pedido-estancia

--- models/pedido.py
+++ models/pedido.py
@@ -23,6 +23,10 @@ class Pedido(models.Model):
             if val.get('name', _('Nuevo Pedido')) == _('Nuevo Pedido'):
                 val['name'] = self.env['ir.sequence'].next_by_code('hotel.pedido') or _('Nuevo Pedido')
         result = super(Pedido, self).create(vals_list)
-        # for rec in result:
-        #     rec.estancia_id.pedidos_ids = [(4, [rec.id])]
+        pedido_nuevo = [result.id]
+        for record in result:
+            for rec in record.estancia_id:
+                pedidos = rec.pedidos_ids.ids
+                todos = pedidos + pedido_nuevo
+                rec.write({'pedidos_ids': [(6, 0, todos)]})
         return result
