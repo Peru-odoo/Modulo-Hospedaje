a6ef22443876e62fa2db44f468559caa9fd941bc
YankielYong <yankielyongmartinez@gmail.com>
2024-02-10 14:22:55 -0500

    agregada la secuencia de salidas

--- models/salida.py
+++ models/salida.py
@@ -11,4 +11,13 @@ class Salida(models.Model):
     _name = 'hotel.salida'
     _description = _('Salida')
 
-    name = fields.Char(_('Name'))
+    name = fields.Char(string=_('Nº'), default=lambda self: _('Registrar Salida'), readonly=True)
+    
+    
+    @api.model_create_multi
+    def create(self, vals_list):
+        for val in vals_list:
+            if val.get('name', _('Registrar Salida')) == _('Registrar Salida'):
+                val['name'] = self.env['ir.sequence'].next_by_code('hotel.salida') or _('Registrar Salida')
+        result = super(Salida, self).create(vals_list)
+        return result
